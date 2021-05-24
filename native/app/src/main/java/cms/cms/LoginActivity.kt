00392a33bca4a874b0ddd3d7006d891b63138811
majczel23000 <michal.razny23@gmail.com>
2021-05-24 23:39:13 +0200

    first kotlin configuration: side-menu, basic screens

---
+++ native/app/src/main/java/cms/cms/LoginActivity.kt
@@ -0,0 +1,20 @@
+package cms.cms
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val btn: Button = findViewById(R.id.button)
+        btn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
